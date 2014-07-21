d168789f99afe5a638f60d2e98352cbf27eb34e7
indotek68 <angga.w.putra@gmail.com>
2014-07-21 16:42:02 -0700

    articles and site routes work! created index.ejs, about.ejs, contact.ejs

--- app.js
+++ app.js
@@ -2,9 +2,37 @@ var express = require('express'),
 bodyParser = require('body-parser'),
 app = express();
 
-
+//homepage
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Hello World'); 
 });
+
+//displays summary
+app.get('/articles', function(req, res){
+  res.send('Summary'); 
+});
+
+//gets a form to save to article
+app.get('/articles/new', function(req, res){
+  res.send('save a new article'); 
+});
+
+//find the article in an array
+app.get('/articles/:id', function(req, res){
+  res.send('find article in array'); 
+});
+
+//static page about the daily planet
+app.get('/about', function(req, res){
+  res.send('about page'); 
+});
+
+//contact
+app.get('/contact', function(req, res){
+  res.send('contact page'); 
+});
+
+
+
 
 app.listen(3000)
